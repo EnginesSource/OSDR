9a12d0c3dae5fc3103b18a83d5a47be3f267ddb4
wazanator <wazanator01@gmail.com>
2014-06-08 23:02:08 -0500

    Added more player models
    Signed-off-by: wazanator <wazanator01@gmail.com>

--- mp/src/game/client/game_controls/playermodelmenu.cpp
+++ mp/src/game/client/game_controls/playermodelmenu.cpp
@@ -19,7 +19,7 @@ extern IFileSystem *g_pFullFileSystem;
 using namespace vgui;
 
 int CURRENTMODEL = 1;
-int MAXMODELS = 20;
+int MAXMODELS = 75;
 
 ConVar cl_showplayermodelpanel("cl_showplayermodelpanel", "0", FCVAR_CLIENTDLL, "Sets the state of player model selection panel <state>");
 
@@ -73,6 +73,7 @@ void CPlayerModelPanel::updateImage(void) {
 	case 1:
 		img = scheme()->GetImage("models/random_model.vmt", false);
 		playerImage->SetImage(img);
+		break;
 	case 2:
 		img = scheme()->GetImage("models/humans/group03/male_01.vmt", false);
 		playerImage->SetImage(img);
@@ -149,6 +150,226 @@ void CPlayerModelPanel::updateImage(void) {
 		img = scheme()->GetImage("models/police.vmt", false);
 		playerImage->SetImage(img);
 		break;
+	case 21:
+		img = scheme()->GetImage("models/hl2arcade/combine_engineer_female_01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 22:
+		img = scheme()->GetImage("models/hl2arcade/combine_engineer_female_02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 23:
+		img = scheme()->GetImage("models/hl2arcade/combine_engineer_female_03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 24:
+		img = scheme()->GetImage("models/hl2arcade/combine_engineer_male_01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 25:
+		img = scheme()->GetImage("models/hl2arcade/combine_engineer_male_02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 26:
+		img = scheme()->GetImage("models/hl2arcade/combine_engineer_male_03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 27:
+		img = scheme()->GetImage("models/hl2arcade/combine_medic_female_01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 28:
+		img = scheme()->GetImage("models/hl2arcade/combine_medic_female_02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 29:
+		img = scheme()->GetImage("models/hl2arcade/combine_medic_female_03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 30:
+		img = scheme()->GetImage("models/hl2arcade/combine_medic_male_01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 31:
+		img = scheme()->GetImage("models/hl2arcade/combine_medic_male_02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 32:
+		img = scheme()->GetImage("models/hl2arcade/combine_medic_male_03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 33:
+		img = scheme()->GetImage("models/hl2arcade/combine_normal_female_01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 34:
+		img = scheme()->GetImage("models/hl2arcade/combine_normal_female_02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 35:
+		img = scheme()->GetImage("models/hl2arcade/combine_normal_female_03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 36:
+		img = scheme()->GetImage("models/hl2arcade/combine_normal_male_01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 37:
+		img = scheme()->GetImage("models/hl2arcade/combine_normal_male_02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 38:
+		img = scheme()->GetImage("models/hl2arcade/combine_normal_male_03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 39:
+		img = scheme()->GetImage("models/hl2arcade/combine_sniper_male_01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 40:
+		img = scheme()->GetImage("models/hl2arcade/combine_sniper_male_02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 41:
+		img = scheme()->GetImage("models/hl2arcade/combine_sniper_male_03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 42:
+		img = scheme()->GetImage("models/hl2arcade/combine_soldier_female_01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 43:
+		img = scheme()->GetImage("models/hl2arcade/combine_soldier_female_02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 44:
+		img = scheme()->GetImage("models/hl2arcade/combine_soldier_female_03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 45:
+		img = scheme()->GetImage("models/hl2arcade/gordon.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 46:
+		img = scheme()->GetImage("models/hl2arcade/resis_engineer_female01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 47:
+		img = scheme()->GetImage("models/hl2arcade/resis_engineer_female02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 48:
+		img = scheme()->GetImage("models/hl2arcade/resis_engineer_female03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 49:
+		img = scheme()->GetImage("models/hl2arcade/resis_engineer_male01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 50:
+		img = scheme()->GetImage("models/hl2arcade/resis_engineer_male02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 51:
+		img = scheme()->GetImage("models/hl2arcade/resis_engineer_male03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 52:
+		img = scheme()->GetImage("models/hl2arcade/resis_medic_female01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 53:
+		img = scheme()->GetImage("models/hl2arcade/resis_medic_female02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 54:
+		img = scheme()->GetImage("models/hl2arcade/resis_medic_female03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 55:
+		img = scheme()->GetImage("models/hl2arcade/resis_medic_male01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 56:
+		img = scheme()->GetImage("models/hl2arcade/resis_medic_male02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 57:
+		img = scheme()->GetImage("models/hl2arcade/resis_medic_male03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 58:
+		img = scheme()->GetImage("models/hl2arcade/resis_normal_female01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 59:
+		img = scheme()->GetImage("models/hl2arcade/resis_normal_female02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 60:
+		img = scheme()->GetImage("models/hl2arcade/resis_normal_female03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 61:
+		img = scheme()->GetImage("models/hl2arcade/resis_normal_male01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 62:
+		img = scheme()->GetImage("models/hl2arcade/resis_normal_male02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 63:
+		img = scheme()->GetImage("models/hl2arcade/resis_normal_male03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 64:
+		img = scheme()->GetImage("models/hl2arcade/resis_sniper_female01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 65:
+		img = scheme()->GetImage("models/hl2arcade/resis_sniper_female02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 66:
+		img = scheme()->GetImage("models/hl2arcade/resis_sniper_female03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 67:
+		img = scheme()->GetImage("models/hl2arcade/resis_sniper_male01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 68:
+		img = scheme()->GetImage("models/hl2arcade/resis_sniper_male02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 69:
+		img = scheme()->GetImage("models/hl2arcade/resis_sniper_male03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 70:
+		img = scheme()->GetImage("models/hl2arcade/resis_soldier_female01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 71:
+		img = scheme()->GetImage("models/hl2arcade/resis_soldier_female02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 72:
+		img = scheme()->GetImage("models/hl2arcade/resis_soldier_female03.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 73:
+		img = scheme()->GetImage("models/hl2arcade/resis_soldier_male01.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 74:
+		img = scheme()->GetImage("models/hl2arcade/resis_soldier_male02.vmt", false);
+		playerImage->SetImage(img);
+		break;
+	case 75:
+		img = scheme()->GetImage("models/hl2arcade/resis_soldier_male03.vmt", false);
+		playerImage->SetImage(img);
+		break;
 	default:
 		break;
 	}
@@ -186,6 +407,7 @@ void CPlayerModelPanel::setModel(void) {
 	case 1:
 		CURRENTMODEL = RandomInt(2, MAXMODELS);
 		setModel();
+		updateImage();
 		break;
 	case 2:
 		engine->ClientCmd("cl_playermodel models/humans/group03/male_01.mdl");
@@ -243,6 +465,171 @@ void CPlayerModelPanel::setModel(void) {
 		break;
 	case 20:
 		engine->ClientCmd("cl_playermodel models/police.mdl");
+		break;
+	case 21:
+		engine->ClientCmd("cl_playermodel models/c_eng_f/combine_engineer_female.mdl");
+		break;
+	case 22:
+		engine->ClientCmd("cl_playermodel models/c_eng_fxss1/c_eng_fxss1.mdl");
+		break;
+	case 23:
+		engine->ClientCmd("cl_playermodel models/c_eng_fxss2/c_eng_fxss2.mdl");
+		break;
+	case 24:
+		engine->ClientCmd("cl_playermodel models/c_eng_m/combine_engineer_male.mdl");
+		break;
+	case 25:
+		engine->ClientCmd("cl_playermodel models/c_eng_mxss1/c_eng_mxss1.mdl");
+		break;
+	case 26:
+		engine->ClientCmd("cl_playermodel models/c_eng_mxss2/c_eng_mxss2.mdl");
+		break;
+	case 27:
+		engine->ClientCmd("cl_playermodel models/c_med_f/combine_medic_female.mdl");
+		break;
+	case 28:
+		engine->ClientCmd("cl_playermodel models/c_med_fxss1/c_med_fxss1.mdl");
+		break;
+	case 29:
+		engine->ClientCmd("cl_playermodel models/c_med_fxss2/c_med_fxss2.mdl");
+		break;
+	case 30:
+		engine->ClientCmd("cl_playermodel models/c_med_m/combine_medic_male.mdl");
+		break;
+	case 31:
+		engine->ClientCmd("cl_playermodel models/c_med_mxss1/c_med_mxss1.mdl");
+		break;
+	case 32:
+		engine->ClientCmd("cl_playermodel models/c_med_mxss2/c_med_mxss2.mdl");
+		break;
+	case 33:
+		engine->ClientCmd("cl_playermodel models/c_nor_f/combine_nomral_female.mdl");
+		break;
+	case 34:
+		engine->ClientCmd("cl_playermodel models/c_nor_fxss1/c_nor_fxss1.mdl");
+		break;
+	case 35:
+		engine->ClientCmd("cl_playermodel models/c_nor_fxss2/c_nor_fxss2.mdl");
+		break;
+	case 36:
+		engine->ClientCmd("cl_playermodel models/c_nor_m/combine_nomral_male.mdl");
+		break;
+	case 37:
+		engine->ClientCmd("cl_playermodel models/c_nor_mxss1/c_nor_mxss1.mdl");
+		break;
+	case 38:
+		engine->ClientCmd("cl_playermodel models/c_nor_mxss2/c_nor_mxss2.mdl");
+		break;
+	case 39:
+		engine->ClientCmd("cl_playermodel models/c_sni_m/combine_sniper_male.mdl");
+		break;
+	case 40:
+		engine->ClientCmd("cl_playermodel models/c_sni_mxss1/c_sni_mxss1.mdl");
+		break;
+	case 41:
+		engine->ClientCmd("cl_playermodel models/c_sni_mxss2/c_sni_mxss2.mdl");
+		break;
+	case 42:
+		engine->ClientCmd("cl_playermodel models/c_sol_f/combine_soldier_female.mdl");
+		break;
+	case 43:
+		engine->ClientCmd("cl_playermodel models/c_sol_fxss1/c_sol_fxss1.mdl");
+		break;
+	case 44:
+		engine->ClientCmd("cl_playermodel models/c_sol_fxss2/c_sol_fxss2.mdl");
+		break;
+	case 45:
+		engine->ClientCmd("cl_playermodel models/gordon/gordon.mdl");
+		break;
+	case 46:
+		engine->ClientCmd("cl_playermodel models/r_eng_f/resis_engineer_female.mdl");
+		break;
+	case 47:
+		engine->ClientCmd("cl_playermodel models/r_eng_fxss1/r_eng_fxss1.mdl");
+		break;
+	case 48:
+		engine->ClientCmd("cl_playermodel models/r_eng_fxss2/r_eng_fxss2.mdl");
+		break;
+	case 49:
+		engine->ClientCmd("cl_playermodel models/r_eng_m/resis_engineer_male.mdl");
+		break;
+	case 50:
+		engine->ClientCmd("cl_playermodel models/r_eng_mxss1/r_eng_mxss1.mdl");
+		break;
+	case 51:
+		engine->ClientCmd("cl_playermodel models/r_eng_mxss2/r_eng_mxss2.mdl");
+		break;
+	case 52:
+		engine->ClientCmd("cl_playermodel models/r_med_f/resis_medic_female.mdl");
+		break;
+	case 53:
+		engine->ClientCmd("cl_playermodel models/r_med_fxss1/r_med_fxss1.mdl");
+		break;
+	case 54:
+		engine->ClientCmd("cl_playermodel models/r_med_fxss2/r_med_fxss2.mdl");
+		break;
+	case 55:
+		engine->ClientCmd("cl_playermodel models/r_med_m/resis_medic_male.mdl");
+		break;
+	case 56:
+		engine->ClientCmd("cl_playermodel models/r_med_mxss1/r_med_mxss1.mdl");
+		break;
+	case 57:
+		engine->ClientCmd("cl_playermodel models/r_med_mxss2/r_med_mxss2.mdl");
+		break;
+	case 58:
+		engine->ClientCmd("cl_playermodel models/r_nor_f/resis_normal_female.mdl");
+		break;
+	case 59:
+		engine->ClientCmd("cl_playermodel models/r_nor_fxss1/r_nor_fxss1.mdl");
+		break;
+	case 60:
+		engine->ClientCmd("cl_playermodel models/r_nor_fxss2/r_nor_fxss2.mdl");
+		break;
+	case 61:
+		engine->ClientCmd("cl_playermodel models/r_nor_m/resis_normal_male.mdl");
+		break;
+	case 62:
+		engine->ClientCmd("cl_playermodel models/r_nor_mxss1/r_nor_mxss1.mdl");
+		break;
+	case 63:
+		engine->ClientCmd("cl_playermodel models/r_nor_mxss2/r_nor_mxss2.mdl");
+		break;
+	case 64:
+		engine->ClientCmd("cl_playermodel models/r_sni_f/resis_sniper_female.mdl");
+		break;
+	case 65:
+		engine->ClientCmd("cl_playermodel models/r_sni_fxss1/r_sni_fxss1.mdl");
+		break;
+	case 66:
+		engine->ClientCmd("cl_playermodel models/r_sni_fxss2/r_sni_fxss2.mdl");
+		break;
+	case 67:
+		engine->ClientCmd("cl_playermodel models/r_sni_m/resis_sniper_male.mdl");
+		break;
+	case 68:
+		engine->ClientCmd("cl_playermodel models/r_sni_mxss1/r_sni_mxss1.mdl");
+		break;
+	case 69:
+		engine->ClientCmd("cl_playermodel models/r_sni_mxss2/r_sni_mxss2.mdl");
+		break;
+	case 70:
+		engine->ClientCmd("cl_playermodel models/r_sol_f/resis_soldier_female.mdl");
+		break;
+	case 71:
+		engine->ClientCmd("cl_playermodel models/r_sol_fxss1/r_sol_fxss1.mdl");
+		break;
+	case 72:
+		engine->ClientCmd("cl_playermodel models/r_sol_fxss2/r_sol_fxss2.mdl");
+		break;
+	case 73:
+		engine->ClientCmd("cl_playermodel models/r_sol_m/resis_soldier_male.mdl");
+		break;
+	case 74:
+		engine->ClientCmd("cl_playermodel models/r_sol_mxss1/r_sol_mxss1.mdl");
+		break;
+	case 75:
+		engine->ClientCmd("cl_playermodel models/r_sol_mxss2/r_sol_mxss2.mdl");
 		break;
 	default:
 		break;
